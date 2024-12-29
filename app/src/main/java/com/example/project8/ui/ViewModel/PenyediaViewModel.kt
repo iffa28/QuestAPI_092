ad3fb27ed93c0cd08625a47f4c0084efea0d544a
Iffatuz Zahra <iffatuz.zahra.ft22@mail.umy.ac.id>
2024-12-29 15:52:54 +0700

    menambahkan PenyediaViewModel untuk mengenalkan viewmodel viewmodel yang ada

--- app/src/main/java/com/example/project8/ui/ViewModel/PenyediaViewModel.kt
+++ app/src/main/java/com/example/project8/ui/ViewModel/PenyediaViewModel.kt
@@ -2,7 +2,26 @@ package com.example.project8.ui.ViewModel
 
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
 import com.example.project8.MahasiswaApplications
+
+object PenyediaViewModel {
+    val Factory = viewModelFactory {
+        initializer {
+            HomeViewModel(
+                aplikasiMahasiswa().container.mahasiswaRepository
+            )
+        }
+
+        initializer {
+            InsertViewModel(
+                aplikasiMahasiswa().container.mahasiswaRepository
+            )
+        }
+
+    }
+}
 
 fun CreationExtras.aplikasiMahasiswa(): MahasiswaApplications =
     (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as MahasiswaApplications)
