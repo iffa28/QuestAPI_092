81b00075c27efe7e461125ab33833a1ef5775e32
Iffatuz Zahra <iffatuz.zahra.ft22@mail.umy.ac.id>
2024-12-29 16:34:23 +0700

    mengubah nama fungsi menjadi CustomTopAppBar

--- app/src/main/java/com/example/project8/ui/customWidget/TopAppBar.kt
+++ app/src/main/java/com/example/project8/ui/customWidget/TopAppBar.kt
@@ -16,7 +16,7 @@ import androidx.compose.ui.Modifier
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun CustomeTopAppBar(
+fun CustomTopAppBar(
     title: String,
     canNavigateBack: Boolean,
     modifier: Modifier = Modifier,
