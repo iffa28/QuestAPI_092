aee571c23235b3a2c40d453b0d1c4698cfcea158
Iffatuz Zahra <iffatuz.zahra.ft22@mail.umy.ac.id>
2024-12-29 15:10:05 +0700

    menambahkan homeviewmodel untuk mengelola data dan state pada halaman home

--- app/src/main/java/com/example/project8/ui/ViewModel/HomeViewModel.kt
+++ app/src/main/java/com/example/project8/ui/ViewModel/HomeViewModel.kt
@@ -1,6 +1,15 @@
 package com.example.project8.ui.ViewModel
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.project8.model.Mahasiswa
+import com.example.project8.repository.MahasiswaRepository
+import kotlinx.coroutines.launch
+import retrofit2.HttpException
+import java.io.IOException
 
 sealed class HomeUiState{
     data class Success(val mahasiswa: List<Mahasiswa>) : HomeUiState()
@@ -8,4 +17,28 @@ sealed class HomeUiState{
     object Loading : HomeUiState()
 }
 
+class HomeViewModel(
+    private val mhs: MahasiswaRepository
+) : ViewModel() {
+    var mhsUIState: HomeUiState by mutableStateOf(HomeUiState.Loading)
+    private set
+
+    init {
+        getMhs()
+    }
+
+    fun getMhs() {
+        viewModelScope.launch {
+            mhsUIState = HomeUiState.Loading
+            mhsUIState = try {
+                HomeUiState.Success(mhs.getMahasiswa())
+            } catch (e: IOException) {
+                HomeUiState.Error
+            } catch (e: HttpException) {
+                HomeUiState.Error
+
+            }
+        }
+    }
+}
 
