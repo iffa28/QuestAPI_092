238b9efd3757b58aa24a42d65408e20300f1270f
Iffatuz Zahra <iffatuz.zahra.ft22@mail.umy.ac.id>
2024-12-30 00:39:02 +0700

    menambahkan alamat navigasi Detail

--- app/src/main/java/com/example/project8/ui/view/DetailView.kt
+++ app/src/main/java/com/example/project8/ui/view/DetailView.kt
@@ -2,18 +2,36 @@ package com.example.project8.ui.view
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.project8.model.Mahasiswa
+import com.example.project8.ui.customWidget.CustomTopAppBar
+import com.example.project8.ui.navigation.DestinasiNavigasi
 import com.example.project8.ui.viewModel.DetailMhsUiState
+import com.example.project8.ui.viewModel.DetailViewModel
+import com.example.project8.ui.viewModel.PenyediaViewModel
+
+object DestinasiDetail : DestinasiNavigasi {
+    override val route = "Detail_mhs"
+    override val titleRes = "Detail Mahasiswa"
+    const val NIM = "nim"
+    val routeWithArgs = "$route/{$NIM}"
+}
+
 
 @Composable
 fun DetailStatus(
