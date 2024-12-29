25a037df5febdaeb87ab6ca3ba7058a7989c9e6a
Iffatuz Zahra <iffatuz.zahra.ft22@mail.umy.ac.id>
2024-12-29 17:15:39 +0700

    Menambahkan fungsi OnError untuk menampilkan tampilan kesalahan dengan opsi retry saat terjadi kegagalan

--- app/src/main/java/com/example/project8/ui/View/HomeView.kt
+++ app/src/main/java/com/example/project8/ui/View/HomeView.kt
@@ -1,5 +1,6 @@
 package com.example.project8.ui.View
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -12,6 +13,7 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
@@ -21,8 +23,33 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import com.example.project8.R
 import com.example.project8.model.Mahasiswa
+
+@Composable
+fun OnError(
+    retryAction:() -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.connection_error), contentDescription = ""
+        )
+        Text(text = stringResource(R.string.loading_failed), modifier = Modifier.padding(16.dp))
+        Button(onClick = retryAction) {
+            Text(stringResource(R.string.retry))
+        }
+
+    }
+
+}
 
 @Composable
 fun MhsLayout(
