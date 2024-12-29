3f1af345118c527f1d003efdeb5b7cbf41162e3b
Iffatuz Zahra <iffatuz.zahra.ft22@mail.umy.ac.id>
2024-12-30 00:21:21 +0700

    menambahkan viewmodel untuk mengelola data dan status UI pada detail mahasiswa

--- app/src/main/java/com/example/project8/ui/viewModel/DetailViewModel.kt
+++ app/src/main/java/com/example/project8/ui/viewModel/DetailViewModel.kt
@@ -1,6 +1,14 @@
 package com.example.project8.ui.viewModel
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.project8.model.Mahasiswa
+import com.example.project8.repository.MahasiswaRepository
+import kotlinx.coroutines.launch
 
 // Kelas sealed untuk merepresentasikan berbagai status UI pada DetailMhs
 sealed class DetailMhsUiState {
@@ -9,3 +17,30 @@ sealed class DetailMhsUiState {
     object Loading : DetailMhsUiState()
 }
 
+// ViewModel untuk mengelola data dan status UI pada DetailMhs
+class DetailViewModel(
+    savedStateHandle: SavedStateHandle,
+    private val mahasiswaRepository: MahasiswaRepository
+) : ViewModel() {
+    private val nim: String = checkNotNull(savedStateHandle["nim"])
+    var detailMhsUiState: DetailMhsUiState by mutableStateOf(DetailMhsUiState.Loading)
+        private set
+
+    init {
+        getMhsbyId()
+    }
+
+    // Fungsi untuk mengambil data mahasiswa berdasarkan NIM
+    fun getMhsbyId() {
+        viewModelScope.launch {
+            detailMhsUiState = DetailMhsUiState.Loading
+            detailMhsUiState = try {
+                DetailMhsUiState.Success(mahasiswaRepository.getMahasiswaById(nim))
+            } catch (e: Exception) {
+                DetailMhsUiState.Error
+            }
+        }
+
+    }
+
+}
