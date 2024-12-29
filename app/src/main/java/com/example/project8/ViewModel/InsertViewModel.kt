44f8ba7b46eb03e827bd235137e4e790897c42c4
Iffatuz Zahra <iffatuz.zahra.ft22@mail.umy.ac.id>
2024-12-29 14:20:36 +0700

    menambahkan event untuk menyimpan input pengguna

---
+++ app/src/main/java/com/example/project8/ViewModel/InsertViewModel.kt
@@ -0,0 +1,10 @@
+package com.example.project8.ViewModel
+
+data class InsertUiEvent(
+    val nim: String = "",
+    val nama: String = "",
+    val alamat: String = "",
+    val jenisKelamin: String = "",
+    val kelas: String = "",
+    val angkatan: String = ""
+)
