21fbcbcbb4831469a0142818d7ca9a3978015820
Iffatuz Zahra <iffatuz.zahra.ft22@mail.umy.ac.id>
2024-12-29 14:27:47 +0700

    menambahkan fungsi yang mengkonversi object mahasiswa menjadi objek InsertUiEvent

--- app/src/main/java/com/example/project8/ViewModel/InsertViewModel.kt
+++ app/src/main/java/com/example/project8/ViewModel/InsertViewModel.kt
@@ -1,5 +1,21 @@
 package com.example.project8.ViewModel
 
+import com.example.project8.model.Mahasiswa
+
+
+fun Mahasiswa.toInsertUiEvent(): InsertUiEvent = InsertUiEvent(
+    nim = nim,
+    nama = nama,
+    alamat = alamat,
+    jenisKelamin = jenisKelamin,
+    kelas = kelas,
+    angkatan = angkatan
+)
+
+data class InsertUiState(
+    val insertUiEvent: InsertUiEvent = InsertUiEvent()
+)
+
 data class InsertUiEvent(
     val nim: String = "",
     val nama: String = "",
@@ -9,6 +25,4 @@ data class InsertUiEvent(
     val angkatan: String = ""
 )
 
-data class InsertUiState(
-    val insertUiEvent: InsertUiEvent = InsertUiEvent()
-)
+
