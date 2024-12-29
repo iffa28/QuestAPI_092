b34313cb76bb9c7ef81852924eabf302658b9297
Iffatuz Zahra <iffatuz.zahra.ft22@mail.umy.ac.id>
2024-12-29 15:30:58 +0700

    menambahkan alamat navigasi untuk halaman insert

---
+++ app/src/main/java/com/example/project8/ui/View/InsertView.kt
@@ -0,0 +1,8 @@
+package com.example.project8.ui.View
+
+import com.example.project8.ui.Navigation.DestinasiNavigasi
+
+object DestinasiEntry: DestinasiNavigasi {
+    override val route = "item_entry"
+    override val titleRes = "Entry Mhs"
+}
