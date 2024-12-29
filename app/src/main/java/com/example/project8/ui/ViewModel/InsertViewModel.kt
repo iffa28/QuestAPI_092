9e1e8d2cfe785c5599cde4145f198bcdab1f1494
Iffatuz Zahra <iffatuz.zahra.ft22@mail.umy.ac.id>
2024-12-29 14:53:26 +0700

    menambahkan InsertViewModel untuk mengelola data dan logika pada layar input

--- app/src/main/java/com/example/project8/ui/ViewModel/InsertViewModel.kt
+++ app/src/main/java/com/example/project8/ui/ViewModel/InsertViewModel.kt
@@ -1,6 +1,35 @@
 package com.example.project8.ui.ViewModel
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.project8.model.Mahasiswa
+import com.example.project8.repository.MahasiswaRepository
+import kotlinx.coroutines.launch
+
+class InsertViewModel(
+    private val mhs: MahasiswaRepository
+): ViewModel() {
+    var uiState by mutableStateOf(InsertUiState())
+    private set
+
+    fun updateInsertMhsState(insertUiEvent: InsertUiEvent){
+        uiState = InsertUiState(insertUiEvent = insertUiEvent)
+    }
+
+    suspend fun insertMhs(){
+        viewModelScope.launch {
+            try {
+                mhs.insertMahasiswa(uiState.insertUiEvent.toMhs())
+            } catch (e: Exception){
+                e.printStackTrace()
+            }
+        }
+    }
+
+}
 
 fun InsertUiEvent.toMhs(): Mahasiswa = Mahasiswa(
     nim = nim,
