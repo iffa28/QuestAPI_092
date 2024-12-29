29bfb0d21be1e97287c3418ad73506bbc7012dd9
Iffatuz Zahra <iffatuz.zahra.ft22@mail.umy.ac.id>
2024-12-30 01:31:06 +0700

    menambahkan fungsi untuk menampilkan tampilan untuk update data mahasiswa

--- app/src/main/java/com/example/project8/ui/view/UpdateView.kt
+++ app/src/main/java/com/example/project8/ui/view/UpdateView.kt
@@ -1,11 +1,55 @@
 package com.example.project8.ui.view
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.project8.ui.customWidget.CustomTopAppBar
 import com.example.project8.ui.navigation.DestinasiNavigasi
+import com.example.project8.ui.viewModel.PenyediaViewModel
+import com.example.project8.ui.viewModel.UpdateViewModel
+import kotlinx.coroutines.launch
 
 object DestinasiUpdate : DestinasiNavigasi {
     override val route = "Update_mhs"
     override val titleRes = "Edit Mahasiswa"
     const val NIM = "nim"
     val routeWithArgs = "$route/{$NIM}"
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun UpdateMhsScreen(
+    navigateBack: () -> Unit,
+    onNavigateUp: () -> Unit,
+    modifier: Modifier = Modifier,
+    updateViewModel: UpdateViewModel = viewModel(factory = PenyediaViewModel.Factory)
+) {
+    val coroutineScope = rememberCoroutineScope()
+    Scaffold(
+        topBar = {
+            CustomTopAppBar(
+                title = "Update Mahasiswa",
+                canNavigateBack = true,
+                navigateUp = navigateBack
+            )
+        },
+        modifier = modifier
+    ) { innerPadding ->
+        EntryBody(
+            insertUiState = updateViewModel.updateMhsUiState,
+            onSiswaValueChange = updateViewModel::updateInsertMhsState,
+            onSaveClick = {
+                coroutineScope.launch {
+                    updateViewModel.updateMhs()
+                    onNavigateUp()
+                }
+            },
+            modifier = Modifier.padding(innerPadding)
+        )
+    }
 }
 
