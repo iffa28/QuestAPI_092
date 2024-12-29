6a46c6cbe2d8a4d3e4c71b84b848f1065d1e7ade
Iffatuz Zahra <iffatuz.zahra.ft22@mail.umy.ac.id>
2024-12-30 00:32:00 +0700

    menambahkan fungsi ItemDetailMhs untuk menampilkan setiap atribut mahasiswa

--- app/src/main/java/com/example/project8/ui/view/DetailView.kt
+++ app/src/main/java/com/example/project8/ui/view/DetailView.kt
@@ -1,14 +1,45 @@
 package com.example.project8.ui.view
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.project8.model.Mahasiswa
 
+@Composable
+fun ItemDetailMhs(
+    modifier: Modifier = Modifier,
+    mahasiswa: Mahasiswa
+)
+{
+    Column(modifier = Modifier.padding(16.dp)
+    ) {
+        ComponentDetailMhs(judul = "Nama Mahasiswa", isinya = mahasiswa.nama)
+        Spacer(modifier = Modifier.padding(4.dp))
+
+        ComponentDetailMhs(judul = "NIM", isinya = mahasiswa.nim)
+        Spacer(modifier = Modifier.padding(4.dp))
+
+        ComponentDetailMhs(judul = "Jenis Kelamin", isinya = mahasiswa.jenisKelamin)
+        Spacer(modifier = Modifier.padding(4.dp))
+
+        ComponentDetailMhs(judul = "Alamat", isinya = mahasiswa.alamat)
+        Spacer(modifier = Modifier.padding(4.dp))
+
+        ComponentDetailMhs(judul = "Kelas", isinya = mahasiswa.kelas)
+        Spacer(modifier = Modifier.padding(4.dp))
+
+        ComponentDetailMhs(judul = "Angkatan", isinya = mahasiswa.angkatan)
+        Spacer(modifier = Modifier.padding(4.dp))
+    }
+}
 
 @Composable
 fun ComponentDetailMhs(
